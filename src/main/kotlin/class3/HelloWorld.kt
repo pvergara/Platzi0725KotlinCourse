88f5780d57f0310598627265ee7aef22dfd1f6ae
pvergara <ecos.logic.org@gmail.com>
2025-07-05 14:43:53 +0200

    Initial commit

--- src/main/kotlin/class3/HelloWorld.kt
+++ src/main/kotlin/class3/HelloWorld.kt
@@ -2,4 +2,19 @@ package class3
 
 fun main() {
     println("Welcome to your email inbox")
+
+    print("System started...")
+    println("Ready")
+
+    println("\n=== EXERCISE ===")
+    println("Let's create your own welcome message that includes:")
+    println("1st - Your name")
+    println("2nd - Your email application name")
+    println("3rd - Motivational message")
+    println("4th - Use at least 2 different println()")
+
+    println("Hello I'm your worse nightmare!")
+    println("Welcome to Hell-mail")
+    println("There's no future")
+    println();println()
 }
